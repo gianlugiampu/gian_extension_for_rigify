a1aa9ac1a08e4d53715d88f3899f5d6e51b9257d
Gianluca Giampuzzo <167550943+gianlugiampu@users.noreply.github.com>
2024-11-09 15:58:14 +0100

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-rigify_info = {
-  'name': "Extensions for Rigify by Gianluca Giampuzzo",
-  'author': "Gianluca Giampuzzo",
-  'version': (2, 1, 0),
-  'blender': (4, 1, 0),
-  'description': "Fully customizable UI Sliders for rigify",
-  'doc_url': "https://github.com/gianlugiampu/gian_extension_for_rigify/blob/main/README.md",
-  'link': "https://github.com/gianlugiampu/gian_extension_for_rigify",
-}
