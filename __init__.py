ad51445c8f26821b985a35f8a8465446b205f79a
Gianluca Giampuzzo <167550943+gianlugiampu@users.noreply.github.com>
2024-11-09 15:54:45 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,6 @@ rigify_info = {
   'version': (2, 1, 0),
   'blender': (4, 1, 0),
   'description': "Fully customizable UI Sliders for rigify",
-  'doc_url': "https://github.com/gianlugiampu/gian_extensions_for_rigify/blob/main/README.md",
-  'link': "https://github.com/gianlugiampu/gian_extensions_for_rigify",
+  'doc_url': "https://github.com/gianlugiampu/gian_extension_for_rigify/blob/main/README.md",
+  'link': "https://github.com/gianlugiampu/gian_extension_for_rigify",
 }
